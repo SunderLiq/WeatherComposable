f1425d06648fcf9e75798ad14c016cba3f4911fe
SunderLiq <57959193+SunderLiq@users.noreply.github.com>
2024-10-04 20:04:05 +0300

    Just cosmetic changes. TODO: Make this work with API

--- app/src/main/java/com/example/appweathercomposable/screens/MainScreen.kt
+++ app/src/main/java/com/example/appweathercomposable/screens/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.appweathercomposable.screens
 
+import android.widget.ImageButton
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,11 +12,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -24,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.appweathercomposable.R
+import com.example.appweathercomposable.ui.theme.Purple40
 import com.example.appweathercomposable.ui.theme.White80
 
 @Preview(showBackground = true)
@@ -46,7 +50,11 @@ fun MainScreen() {
             modifier = Modifier
                 .fillMaxWidth(),
                 elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-            shape = RoundedCornerShape(2.dp)
+            shape = RoundedCornerShape(2.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Purple40,
+                contentColor = Color.White
+            )
                 )
         {
             Column (modifier = Modifier
@@ -56,10 +64,26 @@ fun MainScreen() {
                 Row (modifier = Modifier
                     .fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween){
-                    Text(text = "03.10.2024 21:37",
+                    Text(text = "03.10.2024 23:30",
                     style = TextStyle(fontSize = 15.sp))
                     AsyncImage(model = "https://cdn.weatherapi.com/weather/64x64/night/113.png", contentDescription = "night_icon",
-                        modifier = Modifier.size(50.dp).padding(top = 5.dp))
+                        modifier = Modifier
+                            .size(50.dp)
+                            .padding(top = 5.dp))
+                }
+                Text(text = "Донецк", style = TextStyle(fontSize = 30.sp))
+                Text(text = "36.4°", style = TextStyle(fontSize = 50.sp), modifier = Modifier.padding(top = 10.dp))
+                Text(text = "Солнечно", style = TextStyle(fontSize = 18.sp), modifier = Modifier.padding(top = 2.dp))
+                Row (modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                    Icon(painterResource(id = R.drawable.search_icon_white), contentDescription = "Search_button", modifier = Modifier
+                        .size(30.dp)
+                        .padding(top = 5.dp))
+                    Text(text = "25.5° / 37.2°")
+                    Icon(painterResource(id = R.drawable.refresh_icon_white), contentDescription = "Search_button", modifier = Modifier
+                        .size(30.dp)
+                        .padding(top = 5.dp))
                 }
             }
         }
